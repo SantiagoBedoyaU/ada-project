59c5ae02f087250b7db2a6113092c0879d43b925
SantiagoBedoyaU <santiago.bedoya35419@ucaldas.edu.co>
2024-10-21 17:24:11 -0500

    Adding advance

--- main.py
+++ main.py
@@ -14,16 +14,41 @@ def background_conditions(matrix: list[list[int]], initial_state: int, candidate
     for idx, variable in enumerate(candidate_system):
         active_variable[int(math.pow(2, idx))] = variable == "1"
 
+    """
+        A -> []
+        B -> []
+        C -> []
+        D -> []
+    """
+    print(active_variable)
+
+    # for i in range(0, 17, 8):
+        # print(i)
+    
+    new_matrix = matrix.copy()
     for key, value in active_variable.items():
         if not value:
             if initial_state[int(math.log2(key))] == "0":
+                """
+                    key = 1
+                    for 0 to new_matrix.length do
+                        new_matrix[key:key + key)] -> 1to2, 3to4, 5to6, 7to8
+                        key += 2
+
+                    key = 2
+                    add = 2
+                    for 0 to new_matrix.length do
+                        new_matrix[key:key+add] -> 2to4, 4to6, 8to10
+                        key += 2
+                        
+                """
                 pass
-            else:
-                pass
+    
+    print(len(new_matrix))
+    for row in new_matrix:
+        print(row)
 
 
-
-    # print(active_variable)
 
 
 def main():
