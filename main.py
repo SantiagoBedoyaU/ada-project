f98c55ae1c6955e8fe29733b8d465e46bd3b9be8
Esteban Naranjo <esteban.082436@gmail.com>
2024-10-21 20:51:45 -0500

    Background conditios

--- main.py
+++ main.py
@@ -13,43 +13,33 @@ def background_conditions(matrix: list[list[int]], initial_state: int, candidate
     active_variable = {}
     for idx, variable in enumerate(candidate_system):
         active_variable[int(math.pow(2, idx))] = variable == "1"
-
-    """
-        A -> []
-        B -> []
-        C -> []
-        D -> []
-    """
-    print(active_variable)
-
-    # for i in range(0, 17, 8):
-        # print(i)
-    
-    new_matrix = matrix.copy()
-    for key, value in active_variable.items():
+        
+    for key, value in reversed(list(active_variable.items())):
         if not value:
             if initial_state[int(math.log2(key))] == "0":
-                """
-                    key = 1
-                    for 0 to new_matrix.length do
-                        new_matrix[key:key + key)] -> 1to2, 3to4, 5to6, 7to8
-                        key += 2
+                row = 0
+                add = key
+                matrix = cut_matrix(matrix, row, add, key)
+            else:
+                row = key
+                add = key*2
+                matrix = cut_matrix(matrix, row, add, key) 
 
-                    key = 2
-                    add = 2
-                    for 0 to new_matrix.length do
-                        new_matrix[key:key+add] -> 2to4, 4to6, 8to10
-                        key += 2
-                        
-                """
-                pass
+def cut_matrix(matrix: list[list[int]], row: int, add: int, key: int):
+    new_matrix = []
+    while row < len(matrix):
+        new_matrix = new_matrix + matrix[row:add]
+        row += (key*2)
+        add += (key*2)
+    return new_matrix
+
+def marginalization_per_row(matrix: list[list[int]], subset: str):
+    active_variable = {}
+    for idx, variable in enumerate(subset):
+        active_variable[int(math.pow(2, idx))] = variable == "1"
+        
+            
     
-    print(len(new_matrix))
-    for row in new_matrix:
-        print(row)
-
-
-
 
 def main():
     [initial_state, candidate_system, future_subsystem, present_subsystem] = read_csv('estructura.csv')[0]
