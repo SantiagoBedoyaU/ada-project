9aa320c1344745a125ce5dad8a5c498333155a0a
SantiagoBedoyaU <santiago.bedoya35419@ucaldas.edu.co>
2024-10-23 15:09:47 -0500

    Adding README.md

---
+++ README.md
@@ -0,0 +1,19 @@
+# ADA Project
+
+## Dependencies
+1. Create virtualenv
+```sh
+pip -m venv .venv
+```
+2. Activate virtual env
+```sh
+source .venv/bin/activate
+```
+For Windows
+```sh
+.\.venv\Scripts\activate
+```
+3. Install dependecies
+```sh
+pip install -r requirements.txt
+```
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import csv
 import math
+import texttable
 
 def read_csv(filename: str):
     lines:list[list[str]] = []
@@ -68,7 +69,19 @@ def marginalize_cols(matrix: np.ndarray, initial_state: str, candidate_system: s
                 matrix = np.delete(matrix, delete_rows, 1)
     return matrix
 
-def group_rows(matrix, key: int, cont: int):
+def marginalize_rows(matrix: np.ndarray, initial_state: str, candidate_system: str):
+    active_variable = {}
+    for idx, variable in enumerate(candidate_system):
+        active_variable[int(math.pow(2, idx))] = variable == "1"
+
+    for key, value in reversed(list(active_variable.items())):
+        if not value:
+            cont = int(math.log2(key) + 1)
+            matrix = group_rows(matrix, key, cont)
+
+    return matrix
+
+def group_rows(matrix: np.ndarray, key: int, cont: int):
     matrix = np.array(matrix).astype(float)  # Convierte la matriz a tipo float
     rows_to_delete = []
     for i in range(0, key):
@@ -81,10 +94,11 @@ def group_rows(matrix, key: int, cont: int):
     matrix = np.delete(matrix, rows_to_delete, 0)
     return matrix
 
-def print_matrix(matrix: list[list[int]]):
+def print_matrix(matrix):
+    table = texttable.Texttable()
     for row in matrix:
-        print(row)
-
+        table.add_row(row)
+    print(table.draw())
 
 def main():
     [initial_state, candidate_system, future_subsystem, present_subsystem] = read_csv(
@@ -93,7 +107,7 @@ def main():
     matrix = np.array(read_csv("matrizGuia.csv"))
     matrix = background_conditions(matrix, initial_state, candidate_system)
     matrix = marginalize_cols(matrix, initial_state, candidate_system)
-    pass
-
+    matrix = marginalize_rows(matrix, initial_state, present_subsystem)
+    print_matrix(matrix)
 
 main()
